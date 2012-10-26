3380ea7750404492466ab3f179bfd118c0ea2e5f
CuongNX <cuongnx.9x@gmail.com>
2012-10-27 08:57:18 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dushopping::Application.config.secret_token = '454b62b58c02b7978d3bed47fd372ab4404304642784827a5b5099d3a8f3cabee0c53bd55917bb76d1d2bdef3006baaced91a9b74cefcda2971c0d50596102dd'
